83afdd1c44b37c73bc074d787be3efcafe83bd0a
kdreps <szatikoko@gmail.com>
2026-04-04 16:52:00 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,16 @@
+const products = [
+  { name: "AIR FORCE 1 BIAŁE/CZARNE", price: 65.54, category: "buty", tag: "TOP" },
+  { name: "AIR FORCE 1 x SUPREME", price: 95.54, category: "buty", tag: "TOP" },
+  { name: "AIR FORCE 1 x OFF WHITE", price: 176.13, category: "buty", tag: "TOP" },
+  { name: "AIR FORCE 1 x NOCTA DRAKE", price: 121.59, category: "buty", tag: "TOP" },
+
+  { name: "AIR JORDAN 1 LOW", price: 88.36, category: "buty", tag: "TOP" },
+  { name: "JORDAN 4", price: 201.33, category: "buty", tag: "TOP" },
+
+  { name: "BLUZA POLO RALPH LAUREN", price: 87.77, category: "bluzy", tag: "TOP" },
+  { name: "BLUZA YEEZY GAP", price: 152.65, category: "bluzy", tag: "TOP" },
+  { name: "BLUZA SUPREME", price: 171.76, category: "bluzy", tag: "TOP" },
+
+  { name: "PERFUMY COCO CHANEL 50ML", price: 55.82, category: "perfumy", tag: "TOP" },
+  { name: "PERFUMY CREED 100ML", price: 85.19, category: "perfumy", tag: "TOP" },
+];
